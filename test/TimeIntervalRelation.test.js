b424290910eeee4bb977129f76ee59a9083cc15e
Jan Dockx <jandockx@gmail.com>
2021-04-25 13:50:21 +0200

    create values in test, and use forEach

--- test/TimeIntervalRelation.test.js
+++ test/TimeIntervalRelation.test.js
@@ -1,10 +1,15 @@
 const { NR_OF_RELATIONS, TimeIntervalRelation } = require('../lib/TimeIntervalRelation')
 
+const values = [...Array(NR_OF_RELATIONS).keys()]
+
 describe('TimeIntervalRelation', function () {
-  for (let i = 0; i < NR_OF_RELATIONS; i++) {
-    it(`constructs for ${i}`, function () {
-      const result = new TimeIntervalRelation(i)
-      result.$bitPattern.should.equal(i)
+  describe('VALUES', function () {})
+  describe('constructor', function () {
+    values.forEach(value => {
+      it(`constructs for ${value}`, function () {
+        const result = new TimeIntervalRelation(value)
+        result.$bitPattern.should.equal(value)
+      })
     })
-  }
+  })
 })
