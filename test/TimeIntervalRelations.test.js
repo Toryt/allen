6fc3e336906d16199abdba687655ae41995598db
Jan Dockx <jandockx@gmail.com>
2021-04-25 15:16:54 +0200

    start basic relations

--- test/TimeIntervalRelations.test.js
+++ test/TimeIntervalRelations.test.js
@@ -1,6 +1,6 @@
 const { NR_OF_RELATIONS } = require('../lib/bitPattern')
 const { TimeIntervalRelation } = require('../lib/TimeIntervalRelation')
-const { VALUES } = require('../lib/TimeIntervalRelations')
+const { VALUES, BASIC_RELATIONS } = require('../lib/TimeIntervalRelations')
 
 describe('TimeIntervalRelations', function () {
   describe('VALUES', function () {
@@ -19,6 +19,24 @@ describe('TimeIntervalRelations', function () {
       // optimized to avoid quadratic time
       VALUES.forEach((ar, i) => {
         ar.$bitPattern.should.equal(i)
+      })
+    })
+  })
+  describe('BASIC_RELATIONS', function () {
+    it('is an array', function () {
+      BASIC_RELATIONS.should.be.an.Array()
+    })
+    it('has 13 entries', function () {
+      BASIC_RELATIONS.length.should.equal(13)
+    })
+    it('contains only TimeIntervalRelations', function () {
+      BASIC_RELATIONS.forEach(ar => {
+        ar.should.be.instanceof(TimeIntervalRelation)
+      })
+    })
+    it('has the basic relation at the position of its ordinal', function () {
+      BASIC_RELATIONS.forEach(br => {
+        BASIC_RELATIONS[br.basicRelationOrdinal()].should.strictEqual(br)
       })
     })
   })
