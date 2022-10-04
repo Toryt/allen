55829228e5234713823370b7ec8bd70d067c38a6
Jan Dockx <jandockx@gmail.com>
2022-10-04 09:36:02 +0200

    make all tests work again

--- test/TimeIntervalRelations.test.ts
+++ test/TimeIntervalRelations.test.ts
@@ -2,7 +2,7 @@
 
 import { LetterAlias, TimeIntervalRelation } from '../src/TimeIntervalRelation'
 import {
-  BitPattern,
+  IntervalIntervalRelationBitPattern,
   CONTAINS_BIT_PATTERN,
   DURING_BIT_PATTERN,
   EMPTY_BIT_PATTERN,
@@ -19,13 +19,13 @@ import {
   PRECEDES_BIT_PATTERN,
   STARTED_BY_BIT_PATTERN,
   STARTS_BIT_PATTERN
-} from '../src/bitPattern'
+} from '../src/intervalIntervalRelationBitPattern'
 import 'should'
 
 function testBasicRelation (
   name: string,
   br: TimeIntervalRelation,
-  bp: BitPattern,
+  bp: IntervalIntervalRelationBitPattern,
   visualAlias: TimeIntervalRelation,
   letterAlias: LetterAlias
 ): void {
