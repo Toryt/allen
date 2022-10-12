aab83f9d4b2c94d0b42ed8a765f582ed1ba59bf3
Jan Dockx <jandockx@gmail.com>
2022-10-12 23:29:36 +0200

    fix arePointsOfSameType (untested)

--- src/util.ts
+++ src/util.ts
@@ -1,14 +1,22 @@
 export function arePointsOfSameType (...p: unknown[]) {
-  return (
-    Array.isArray(p) &&
-    p.every(
-      (e, i) =>
-        e === undefined ||
-        e === null ||
-        (!Number.isNaN(e) &&
-          typeof e !== 'symbol' &&
-          (i === 0 ||
-            (typeof e === typeof p[i - 1] && (typeof p[i - 1] !== 'object' || e instanceof p[i - 1].constructor))))
-    )
+  if (!Array.isArray(p)) {
+    return false
+  }
+  const { result } = p.reduce(
+    ({ type, result }, e) => {
+      if (!result || Number.isNaN(e) || typeof e === 'symbol') {
+        return { type, result: false }
+      }
+      if (e === undefined || e === null) {
+        return { type, result }
+      }
+      if (type === undefined) {
+        const eType = typeof e
+        return { type: eType === 'object' ? e.constructor : eType, result }
+      }
+      return { type, result: (typeof type === 'string' && typeof e === type) || e instanceof type }
+    },
+    { type: undefined, result: true }
   )
+  return result
 }
