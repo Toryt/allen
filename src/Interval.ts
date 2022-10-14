dd9acf553b171ae7c99196fd335db6447f5e7d3e
Jan Dockx <jandockx@gmail.com>
2022-10-14 09:01:40 +0200

    add type safe common type, and isInterval2 (untested, not sure if this makes sense)

--- src/Interval.ts
+++ src/Interval.ts
@@ -1,15 +1,36 @@
-import { arePointsOfSameType } from './util'
+import { arePointsOfSameType, commonType, Constructor, primitiveTypes, TypeResult } from './util'
 
 export interface Interval<T> {
   readonly start?: T | undefined
   readonly end?: T | undefined
 }
 
-export function isInterval (i: unknown): boolean {
+export function isInterval (i: unknown): i is Interval<TypeResult> {
   if (i === undefined || i === null || typeof i !== 'object') {
     return false
   }
 
   const pI = i as Partial<Interval<unknown>>
   return arePointsOfSameType(pI.start, pI.end)
+}
+
+export function isInterval2<T extends TypeResult> (
+  i: unknown,
+  type: T extends typeof primitiveTypes[number] ? T : Constructor<T>
+): i is Interval<T> {
+  if (i === undefined || i === null || typeof i !== 'object') {
+    return false
+  }
+
+  const pI = i as Partial<Interval<unknown>>
+  const cType = commonType(pI.start, pI.end)
+  if (cType === false) {
+    return false
+  }
+  if (cType === undefined) {
+    return true
+  }
+  return (
+    cType === type || (typeof cType === 'function' && typeof type === 'function' && cType.prototype instanceof type)
+  )
 }
--- src/util.ts
+++ src/util.ts
@@ -37,3 +37,56 @@ export function arePointsOfSameType (...p: unknown[]): boolean {
   )
   return result
 }
+
+export const primitiveTypes = ['number', 'bigint', 'string', 'boolean'] as const
+export type Constructor<T extends Object> = new (...args: never[]) => T
+export type TypeResult = typeof primitiveTypes[number] | Constructor<Object>
+
+export function commonType (...p: unknown[]): TypeResult | undefined | false {
+  const { type, result } = p.reduce(
+    ({ typeDefiner, type, result }: Acc, e: unknown): Acc => {
+      if (result === false || Number.isNaN(e) || typeof e === 'symbol') {
+        // there is no common type: return false
+        return { result: false }
+      }
+      if (e === undefined || e === null) {
+        // e does not define the common type: continue
+        return { typeDefiner, type, result }
+      }
+      if (type === undefined) {
+        // first occurence of a value that might determine the common type
+        const type: TypeResult =
+          typeof e === 'object' ? (e.constructor as Constructor<Object>) : (typeof e as typeof primitiveTypes[number])
+        // assert(type1 !== 'undefined')
+        // assert(type1 !== 'symbol')
+        // assert(type1 !== 'object')
+        // assert(type1 !== 'function') // MUDO
+        return { typeDefiner: e, type, result }
+      }
+      if (typeof type !== 'function') {
+        // we expect a primitive type: is e of that type?
+        if (typeof e !== type) {
+          return { result: false }
+        }
+        return {
+          typeDefiner,
+          type,
+          result
+        }
+      }
+      // assert(typeDefiner !== null)
+      if (typeDefiner instanceof e.constructor) {
+        // e is a supertype of typeDefiner: switch
+        return { typeDefiner: e, type: e.constructor as Constructor<Object>, result }
+      }
+      if (e instanceof type) {
+        // typeDefiner is a supertype of e: continue
+        return { typeDefiner, type, result }
+      }
+      // e and typeDefiner are unrelated: there is no common type: return false
+      return { result: false }
+    },
+    { result: true }
+  )
+  return result && type
+}
