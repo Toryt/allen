4d095c393f46a1a9d00545f64726caeadd64e5fc
Jan Dockx <jandockx@gmail.com>
2022-10-15 14:24:41 +0200

    introduce and test pointTypeOf

--- src/point.ts
+++ src/point.ts
@@ -6,3 +6,44 @@
  */
 export const primitivePointTypes = ['number', 'bigint', 'string', 'boolean', 'function'] as const
 
+/**
+ * When an `object` is used as point, it's precise type is expressed by its constructor.
+ *
+ * Any constructor represents a possible point type.
+ */
+export type Constructor<T extends Object> = new (...args: never[]) => T
+
+/**
+ * _Dynamic representation_ of the type of a definite point.
+ *
+ * For primitive types, this is the `typeof` string. For `objects`, it is the constructor.
+ */
+export type DefinitePointTypeRepresentation = typeof primitivePointTypes[number] | Constructor<Object>
+
+/**
+ * _Dynamic representation_ of the type of a point, i.e., {@link DefinitePointTypeRepresentation} or
+ * `undefined`.
+ *
+ * When the point is `undefined` or `null`, to express “don't know 🤷”, its point type representation
+ * is `undefined`.
+ */
+export type PointTypeRepresentation = DefinitePointTypeRepresentation | undefined
+
+/**
+ * The dynamic representation of the precise point type.
+ *
+ * Returns `undefined` when `p` is `undefined` or `null`, expressing “don't know 🤷”.
+ * Returns `false` when `p` is not an acceptable point, i.e., when `p` is `NaN`, or a `symbol`.
+ */
+export function pointTypeOf (u: unknown): PointTypeRepresentation | false {
+  if (u === undefined || u === null) {
+    return undefined
+  }
+  const typeOfU = typeof u
+  assert(typeOfU !== 'undefined')
+  if (typeOfU === 'symbol' || Number.isNaN(u)) {
+    return false
+  }
+  return typeOfU === 'object' ? (u.constructor as Constructor<Object>) : typeOfU
+}
+
--- test/point.test.ts
+++ test/point.test.ts
@@ -1,7 +1,8 @@
 /* eslint-env mocha */
 
-import 'should'
-import { primitivePointTypes } from '../src/point'
+import should from 'should'
+import { pointTypeOf, primitivePointTypes } from '../src/point'
+import { inspect } from 'util'
 
 describe('point', function () {
   describe('primitivePointTypes', function () {
@@ -17,6 +18,80 @@ describe('point', function () {
     notPrimitivePointTypes.forEach(nppt => {
       it(`${nppt} is not a primitive point type`, function () {
         ppts.includes(nppt).should.be.false()
+      })
+    })
+  })
+  describe('pointTypeOf', function () {
+    describe('false result', function () {
+      const falseCases = [NaN, Symbol('not a point')]
+      falseCases.forEach(c => {
+        it(`returns false for ${inspect(c)}`, function () {
+          should(pointTypeOf(c)).be.false()
+        })
+      })
+    })
+    describe("don't know 🤷", function () {
+      const dontKnowCases = [undefined, null]
+      dontKnowCases.forEach(c => {
+        it(`returns undefined for ${inspect(c)}`, function () {
+          should(pointTypeOf(c)).be.undefined()
+        })
+      })
+    })
+  })
+  describe('primitive an wrapper types', function () {
+    const primitiveCases = [
+      Number.NEGATIVE_INFINITY,
+      Number.MIN_VALUE,
+      Number.MIN_SAFE_INTEGER,
+      -34,
+      0,
+      Number.EPSILON,
+      1,
+      Math.LN2,
+      Math.PI,
+      45,
+      Number.MAX_SAFE_INTEGER,
+      Number.MAX_VALUE,
+      Number.POSITIVE_INFINITY,
+      -895672890643709647309687348967934n,
+      -34n,
+      -1n,
+      0n,
+      1n,
+      89679867134989671398678136983476983476982679234n,
+      'a string',
+      '   an untrimmed string    ',
+      '',
+      true,
+      false,
+      function () {
+        return true
+      },
+      (a: string) => a + '!',
+      Number(4),
+      BigInt(4),
+      String('a wrapped string'),
+      Boolean(false),
+      Number,
+      BigInt,
+      String,
+      Boolean,
+      Array,
+      Date,
+      Object
+    ]
+    primitiveCases.forEach(c => {
+      it(`returns ${typeof c} for ${inspect(c)}`, function () {
+        should(pointTypeOf(c)).equal(typeof c)
+      })
+    })
+  })
+  describe('objects', function () {
+    const objectCases = [new Date(89673548), {}, { a: 'an object' }, [], [1, 2], [1, {}], Math, JSON]
+    objectCases.forEach(c => {
+      it(`returns the constructor for ${inspect(c)}`, function () {
+        should(pointTypeOf(c)).equal(c.constructor)
       })
     })
   })
