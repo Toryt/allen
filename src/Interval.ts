250d6c59b59b8048e959495e4d503ed985eac438
Jan Dockx <jandockx@gmail.com>
2022-10-16 01:16:10 +0200

    work on Interval, isInterval (untested)

--- src/Interval.ts
+++ src/Interval.ts
@@ -1,36 +1,27 @@
-import { arePointsOfSameType, commonType, Constructor, primitiveTypes, TypeResult } from './util'
+import { commonType, mostSpecializedCommonType } from './util'
+import { Indefinite, Point, PointTypeFor, PointTypeRepresentation } from './point'
 
-export interface Interval<T> {
-  readonly start?: T | undefined
-  readonly end?: T | undefined
+export interface Interval<T extends Point> {
+  readonly start?: Indefinite<T>
+  readonly end?: Indefinite<T>
 }
 
-export function isInterval (i: unknown): i is Interval<TypeResult> {
+export function isInterval (i: unknown): i is Interval<Point>
+export function isInterval<T extends PointTypeRepresentation> (i: unknown, pointType: T): i is Interval<PointTypeFor<T>>
+export function isInterval (i: unknown, pointType?: PointTypeRepresentation): boolean {
   if (i === undefined || i === null || typeof i !== 'object') {
     return false
   }
 
-  const pI = i as Partial<Interval<unknown>>
-  return arePointsOfSameType(pI.start, pI.end)
-}
-
-export function isInterval2<T extends TypeResult> (
-  i: unknown,
-  type: T extends typeof primitiveTypes[number] ? T : Constructor<T>
-): i is Interval<T> {
-  if (i === undefined || i === null || typeof i !== 'object') {
-    return false
-  }
-
-  const pI = i as Partial<Interval<unknown>>
+  const pI = i as Partial<Interval<Point>>
   const cType = commonType(pI.start, pI.end)
-  if (cType === false) {
-    return false
-  }
-  if (cType === undefined) {
-    return true
-  }
   return (
-    cType === type || (typeof cType === 'function' && typeof type === 'function' && cType.prototype instanceof type)
+    cType !== false &&
+    (cType === undefined ||
+      pointType === undefined ||
+      cType === pointType ||
+      (typeof cType === 'function' &&
+        typeof pointType === 'function' &&
+        mostSpecializedCommonType(cType, pointType) === pointType))
   )
 }
