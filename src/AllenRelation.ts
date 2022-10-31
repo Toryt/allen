d050a97da0138aa9366aafd342291e76b69602c5
Jan Dockx <jandockx@gmail.com>
2022-10-31 13:51:45 +0100

    change AllenRelation to use generalization and generalized tests
    failures!

--- src/AllenRelation.ts
+++ src/AllenRelation.ts
@@ -1,185 +1,68 @@
-import {
-  NR_OF_RELATIONS as BIT_PATTERN_NR_OF_RELATIONS,
-  allenRelationBitPatterns,
-  EMPTY_BIT_PATTERN,
-  FULL_BIT_PATTERN,
-  isBasicAllenRelationBitPattern,
-  basicAllenRelationBitPatterns
-} from './allenRelationBitPattern'
-import assert from 'assert'
+import { Relation } from './Relation'
+import { basicRelationBitPatterns, relationBitPatterns } from './relationBitPattern'
 
 /**
  * Support for reasoning about Allen relations, i.e., relations between intervals, and constraints on those relations.
  *
  * **We strongly advise to use this class when working with relations between intervals. Reasoning about relations
  * between intervals is treacherously difficult.**
- *
- * ### About the code
- *
- * We have chosen to introduce a full-featured type for working with interval relations, although they are just bit
- * patterns in essence, to make encapsulation as good as possible. This has a slight performance overhead, but we
- * believe that this is worth it, considering the immense complexity of reasoning about relations between intervals.
- *
- * Allen relations follow the ‘8192-fold enumeration pattern’. All possible instances are created when this module is
- * loaded, and it is impossible for a user to create new instances. This means that reference equality (‘`===`’) can be
- * used to compare Allen relations. Instances are obtained using the constants this module offers, or using
- *
- * - the combination methods
- *   - {@link or},
- *   - {@link and},
- *   - {@link min},
- *   - {@link compose}, and // MUDO make instance method?
- * - the unary method {@link complement}.
- * // MUDO missing one!
- *
- * Basic Allen relations are instances of the subclass {@link BasicAllenRelation}. For technical reasons,
- * {@link BasicAllenRelation.RELATIONS}, the lists all possible Allen relations, is a static property of that class, not
- * this.
- *
- * All instance methods in this class are _O(1)_, i.e., work in constant time, and all static methods are _O(n)_, i.e.,
- * work in linear time.
  */
-export class AllenRelation {
-  /* Implementation note:
-
-     Allen relations are implemented as a 13-bit bit pattern, stored in the 13 least significant bits of an integer
-     number.
-
-     Each of those 13 bits represents a basic relation, being in the general relation (`1`) or not being in the general
-     relation (`0`), where the relation is thought of as a set of basic relations.
-
-     The order of the basic relations in the bit pattern is important for some algorithms. There is some trickery
-     involved. */
-
-  /**
-   * Only the 5 lowest bits are used. The other (32 - 5 = 27 bits) are 0.
-   */
-  protected readonly bitPattern: number
-
-  protected constructor (bitpattern: number) {
-    /**
-     * Only the 13 lowest bits are used. The other (32 - 13 = 19 bits) are 0.
-     */
-    this.bitPattern = bitpattern
-  }
+export class AllenRelation extends Relation {
+  public static readonly NR_OF_BITS = 13
+  public static readonly BASIC_REPRESENTATIONS = Object.freeze([
+    'p',
+    'm',
+    'o',
+    'F',
+    'D',
+    's',
+    'e',
+    'S',
+    'd',
+    'f',
+    'O',
+    'M',
+    'P'
+  ] as const)
 
   /**
-   * Is `this` implied by `gr`?
-   *
-   * In other words, when considering the relations as a set
-   * of basic relations, is `this` a superset of `gr` (considering equality as also acceptable)?
-   *
-   * @basic
-   * @pre gr instanceof TimeInterval
-   * @invar this.impliedBy(this)
-   * @invar !this.isBasic() || BASIC_RELATIONS.every(br => br === this || !this.impliedBy(br))
-   * @invar VALUES.every(gr => this.impliedBy(gr) === BASIC_RELATIONS.every(br => !gr.impliedBy(br) || this.impliedBy(br)))
-   * @return {boolean}
-   */
-  impliedBy (gr: AllenRelation): boolean {
-    return (this.bitPattern & gr.bitPattern) === gr.bitPattern
-  }
-}
-
-/**
- * The total number of possible Allen relations **= 8192**
- * (i.e., <code>2<sup>13</sup></code>).
- */
-export const NR_OF_RELATIONS: number = BIT_PATTERN_NR_OF_RELATIONS
-
-export const BASIC_ALLEN_RELATION_REPRESENTATIONS = Object.freeze([
-  'p',
-  'm',
-  'o',
-  'F',
-  'D',
-  's',
-  'e',
-  'S',
-  'd',
-  'f',
-  'O',
-  'M',
-  'P'
-] as const)
-export type BasicAllenRelationRepresentation = typeof BASIC_ALLEN_RELATION_REPRESENTATIONS[number]
-
-export class BasicAllenRelation extends AllenRelation {
-  /**
-   * ### Invariants
-   *
-   * ```
-   * this.representation === BASIC_ALLEN_RELATION_REPRESENTATIONS[this.ordinal()]
-   */
-  public readonly representation: BasicAllenRelationRepresentation
-
-  /**
-   * There is only 1 constructor, that constructs the wrapper object
-   * around the bitpattern. This is used exclusively in {@link BASIC_RELATIONS} initialization code.
-   */
-  private constructor (bitPattern: number) {
-    assert(bitPattern >= EMPTY_BIT_PATTERN)
-    assert(bitPattern <= FULL_BIT_PATTERN)
-
-    super(bitPattern)
-    this.representation = BASIC_ALLEN_RELATION_REPRESENTATIONS[this.ordinal()]
-  }
-
-  /**
-   * An ordinal for basic relations (zero-based).
+   * All possible Allen relations.
    *
    * ### Invariants
    *
    * ```
-   * BASIC_RELATIONS[this.ordinal()] === this
+   * Array.isArray(RELATIONS)
+   * RELATIONS.length === NR_OF_RELATIONS
+   * RELATIONS.every(gr => gr instanceof AllenRelation)
+   * RELATIONS.every((gr1, i1) => RELATIONS.every((gr2, i2) => i2 <= i1 || gr1 !== gr2))
    * ```
+   *
+   * There are no other `AllenRelation`s than the instances of this array.
    */
-  public ordinal (): number {
-    /*
-     * This is the bit position, 0-based, in the 5-bit bit pattern, of the bit
-     * representing this as basic relation.
-     *
-     * See https://www.geeksforgeeks.org/position-of-rightmost-set-bit/
-     */
-    return Math.log2(this.bitPattern & -this.bitPattern)
-  }
+  public static readonly RELATIONS: readonly AllenRelation[] = Object.freeze(
+    relationBitPatterns(this.NR_OF_BITS).map(bitPattern => new AllenRelation(bitPattern))
+  )
 
   /**
    * All possible basic Allen relations.
    *
-   * This is a _basis_ for all Allen relations.
-   *
-   * That they are presented here in a particular order, is a pleasant side note, but in general not relevant for the
-   * user. The list is ordered, first on the first interval beginning before the second (`I1.start [< = >] I2.start`)
-   * and secondly on the first interval ending before the second (`I1.end [< = >] I2.end`).
+   * This is an _orthogonal basis_ for all Allen relations.
    *
    * ### Invariants
    *
    * ```
    * Array.isArray(BASIC_RELATIONS)
-   * BASIC_RELATIONS.length === 13
-   * BASIC_RELATIONS.every(br => br instanceof BasicAllenRelation)
+   * BASIC_RELATIONS.length === 5
+   * BASIC_RELATIONS.every(br => br instanceof AllenRelation)
+   * BASIC_RELATIONS.every(br => RELATIONS.includes(br))
    * BASIC_RELATIONS.every((br1, i1) =>
    *   BASIC_RELATIONS.every((br2, i2) => i2 <= i1 || br1 !== br2 && !br1.implies(br2) && !br2.implies(br1)))
-   * BASIC_RELATIONS[ 0] == PRECEDES
-   * BASIC_RELATIONS[ 1] == MEETS
-   * BASIC_RELATIONS[ 2] == OVERLAPS
-   * BASIC_RELATIONS[ 3] == FINISHED_BY
-   * BASIC_RELATIONS[ 4] == CONTAINS
-   * BASIC_RELATIONS[ 5] == STARTS
-   * BASIC_RELATIONS[ 6] == EQUALS
-   * BASIC_RELATIONS[ 7] == STARTED_BY
-   * BASIC_RELATIONS[ 8] == DURING
-   * BASIC_RELATIONS[ 9] == FINISHES
-   * BASIC_RELATIONS[10] == OVERLAPPED_BY
-   * BASIC_RELATIONS[11] == MET_BY
-   * BASIC_RELATIONS[12] == PRECEDED_BY
    * ```
    *
-   * There are no other `BasicAllenRelation`s than the instances of this array.
+   * There are no other basic `AllenRelation`s than the instances of this array.
    */
-  public static readonly BASIC_RELATIONS: readonly BasicAllenRelation[] = Object.freeze(
-    basicAllenRelationBitPatterns.map(bitPattern => new BasicAllenRelation(bitPattern))
+  public static readonly BASIC_RELATIONS: readonly AllenRelation[] = Object.freeze(
+    basicRelationBitPatterns(this.NR_OF_BITS).map(bitPattern => AllenRelation.RELATIONS[bitPattern])
   )
 
   /**
@@ -197,7 +80,7 @@ export class BasicAllenRelation extends AllenRelation {
    *
    * The converse of this relation is {@link PRECEDED_BY}.
    */
-  static readonly PRECEDES: BasicAllenRelation = BasicAllenRelation.BASIC_RELATIONS[0]
+  static readonly PRECEDES: AllenRelation = AllenRelation.BASIC_RELATIONS[0]
   // Bit pattern: 1 = '0000000000001'
 
   /**
@@ -215,7 +98,7 @@ export class BasicAllenRelation extends AllenRelation {
    *
    * The converse of this relation is {@link MET_BY}.
    */
-  static readonly MEETS: BasicAllenRelation = BasicAllenRelation.BASIC_RELATIONS[1]
+  static readonly MEETS: AllenRelation = AllenRelation.BASIC_RELATIONS[1]
   // Bit pattern: 2 = '0000000000010'
 
   /**
@@ -235,7 +118,7 @@ export class BasicAllenRelation extends AllenRelation {
    *
    * The converse of this relation is {@link OVERLAPPED_BY}.
    */
-  static readonly OVERLAPS: BasicAllenRelation = BasicAllenRelation.BASIC_RELATIONS[2]
+  static readonly OVERLAPS: AllenRelation = AllenRelation.BASIC_RELATIONS[2]
   // Bit pattern: 4 = '0000000000100'
 
   /**
@@ -255,7 +138,7 @@ export class BasicAllenRelation extends AllenRelation {
    *
    * The converse of this relation is {@link FINISHED_BY}.
    */
-  static readonly FINISHED_BY: BasicAllenRelation = BasicAllenRelation.BASIC_RELATIONS[3]
+  static readonly FINISHED_BY: AllenRelation = AllenRelation.BASIC_RELATIONS[3]
   // Bit pattern: 8 = '0000000001000'
 
   /**
@@ -275,7 +158,7 @@ export class BasicAllenRelation extends AllenRelation {
    *
    * The converse of this relation is {@link DURING}.
    */
-  static readonly CONTAINS: BasicAllenRelation = BasicAllenRelation.BASIC_RELATIONS[4]
+  static readonly CONTAINS: AllenRelation = AllenRelation.BASIC_RELATIONS[4]
   // Bit pattern: 16 = '0000000010000'
 
   /**
@@ -295,7 +178,7 @@ export class BasicAllenRelation extends AllenRelation {
    *
    * The converse of this relation is {@link STARTED_BY}.
    */
-  static readonly STARTS: BasicAllenRelation = BasicAllenRelation.BASIC_RELATIONS[5]
+  static readonly STARTS: AllenRelation = AllenRelation.BASIC_RELATIONS[5]
   // Bit pattern: 32 = '0000000100000'
 
   /**
@@ -315,7 +198,7 @@ export class BasicAllenRelation extends AllenRelation {
    *
    * The converse of this relation is itself.
    */
-  static readonly EQUALS: BasicAllenRelation = BasicAllenRelation.BASIC_RELATIONS[6]
+  static readonly EQUALS: AllenRelation = AllenRelation.BASIC_RELATIONS[6]
   // Bit pattern: 64 = '0000001000000'
 
   /**
@@ -335,7 +218,7 @@ export class BasicAllenRelation extends AllenRelation {
    *
    * The converse of this relation is {@link STARTS}.
    */
-  static readonly STARTED_BY: BasicAllenRelation = BasicAllenRelation.BASIC_RELATIONS[7]
+  static readonly STARTED_BY: AllenRelation = AllenRelation.BASIC_RELATIONS[7]
   // Bit pattern: 128 = '0000010000000'
 
   /**
@@ -355,7 +238,7 @@ export class BasicAllenRelation extends AllenRelation {
    *
    * The converse of this relation is {@link CONTAINS}.
    */
-  static readonly DURING: BasicAllenRelation = BasicAllenRelation.BASIC_RELATIONS[8]
+  static readonly DURING: AllenRelation = AllenRelation.BASIC_RELATIONS[8]
   // Bit pattern: 256 = '0000100000000'
 
   /**
@@ -375,7 +258,7 @@ export class BasicAllenRelation extends AllenRelation {
    *
    * The converse of this relation is {@link FINISHES}.
    */
-  static readonly FINISHES: BasicAllenRelation = BasicAllenRelation.BASIC_RELATIONS[9]
+  static readonly FINISHES: AllenRelation = AllenRelation.BASIC_RELATIONS[9]
   // Bit pattern: 512 = '0001000000000'
 
   /**
@@ -395,7 +278,7 @@ export class BasicAllenRelation extends AllenRelation {
    * The conventional short representation of this Allen relation is &quot;<code><strong>O</strong></code>&quot;.
    * The converse of this relation is {@link OVERLAPS}.
    */
-  static readonly OVERLAPPED_BY: BasicAllenRelation = BasicAllenRelation.BASIC_RELATIONS[10]
+  static readonly OVERLAPPED_BY: AllenRelation = AllenRelation.BASIC_RELATIONS[10]
   // Bit pattern: 1024 = '0010000000000'
 
   /**
@@ -413,7 +296,7 @@ export class BasicAllenRelation extends AllenRelation {
    *
    * The converse of this relation is {@link MEETS}.
    */
-  static readonly MET_BY: BasicAllenRelation = BasicAllenRelation.BASIC_RELATIONS[11]
+  static readonly MET_BY: AllenRelation = AllenRelation.BASIC_RELATIONS[11]
   // Bit pattern: 2048 = '0100000000000'
 
   /**
@@ -431,59 +314,6 @@ export class BasicAllenRelation extends AllenRelation {
    *
    * The converse of this relation is {@link PRECEDES}.
    */
-  static readonly PRECEDED_BY: BasicAllenRelation = BasicAllenRelation.BASIC_RELATIONS[12]
+  static readonly PRECEDED_BY: AllenRelation = AllenRelation.BASIC_RELATIONS[12]
   // Bit pattern: 4069 = '1000000000000'
-
-  /**
-   * All possible Allen relations.
-   *
-   * ### Invariants
-   *
-   * ```
-   * Array.isArray(RELATIONS)
-   * RELATIONS.length === NR_OF_RELATIONS
-   * RELATIONS.every(gr => gr instanceof AllenRelation)
-   * RELATIONS.every((gr1, i1) => RELATIONS.every((gr2, i2) => i2 <= i1 || gr1 !== gr2))
-   * BASIC_RELATIONS.every(br => RELATIONS.includes(br))
-   * ```
-   *
-   * There are no other `AllenRelation`s than the instances of this array.
-   */
-  public static readonly RELATIONS: readonly AllenRelation[] = Object.freeze(
-    allenRelationBitPatterns.map(bitPattern =>
-      isBasicAllenRelationBitPattern(bitPattern)
-        ? BasicAllenRelation.BASIC_RELATIONS[Math.log2(bitPattern & -bitPattern)]
-        : new AllenRelation(bitPattern)
-    )
-  )
 }
-
-/**
- * This empty relation is not a true Allen relation. It does not express a relational condition between intervals. Yet,
- * it is needed for consistency with some operations on Allen interval relations.
- *
- * The converse of this relation is the relation itself.
- *
- * ### Invariants
- *
- * ```
- * BasicAllenRelation.RELATIONS.every(gr => gr === EMPTY || !EMPTY.impliedBy(gr))
- * ```
- */
-export const EMPTY: AllenRelation = BasicAllenRelation.RELATIONS[EMPTY_BIT_PATTERN]
-// Bit pattern: 0 = '0000000000000'
-
-/**
- * The full Allen relation, which expresses that nothing definite can be said about the relation between 2 intervals.
- *
- * The converse of this relation is the relation itself.
- *
- * ### Invariants
- *
- * ```
- * BasicAllenRelation.RELATIONS.every(gr => FULL.impliedBy(gr))
- * FULL === or(PRECEDES, MEETS, OVERLAPS, FINISHED_BY, CONTAINS, STARTS, EQUALS, STARTED_BY, DURING, FINISHES, OVERLAPPED_BY, MET_BY, PRECEDED_BY)
- * ```
- */
-export const FULL: AllenRelation = BasicAllenRelation.RELATIONS[FULL_BIT_PATTERN]
-// Bit pattern: 31 = '11111'
--- test/65.AllenRelation.test.ts
+++ test/65.AllenRelation.test.ts
@@ -1,143 +1,23 @@
 /* eslint-env mocha */
 
-import { NR_OF_BITS, NR_OF_RELATIONS as BITPATTERN_NR_OF_RELATIONS } from '../src/allenRelationBitPattern'
-import {
-  NR_OF_RELATIONS,
-  AllenRelation,
-  BasicAllenRelation,
-  BASIC_ALLEN_RELATION_REPRESENTATIONS,
-  EMPTY,
-  FULL
-} from '../src/AllenRelation'
+import { AllenRelation } from '../src/AllenRelation'
 import 'should'
-
-function testBasicRelation (name: string, br: BasicAllenRelation, ordinal: number, representation: string): void {
-  describe(name, function () {
-    it('is a BasicAllenRelation', function () {
-      br.should.be.instanceof(BasicAllenRelation)
-    })
-    it(`has ${ordinal} as ordinal`, function () {
-      br.ordinal().should.equal(ordinal)
-    })
-    it('has an integer ordinal [0, 13[', function () {
-      const o = br.ordinal()
-      Number.isInteger(o).should.be.true()
-      o.should.be.greaterThanOrEqual(0)
-      o.should.be.lessThan(NR_OF_BITS)
-    })
-    it(`has ${BASIC_ALLEN_RELATION_REPRESENTATIONS[ordinal]} as representation`, function () {
-      br.representation.should.equal(BASIC_ALLEN_RELATION_REPRESENTATIONS[ordinal])
-    })
-    it('is in BASIC_RELATIONS at the position of its ordinal', function () {
-      BasicAllenRelation.BASIC_RELATIONS[br.ordinal()].should.equal(br)
-    })
-    it(`has '${representation}' as representation`, function () {
-      br.representation.should.equal(representation)
-      br.representation.should.equal(BASIC_ALLEN_RELATION_REPRESENTATIONS[br.ordinal()])
-    })
-  })
-}
+import { generateRelationTests } from './_generateRelationTests'
 
 describe('AllenRelation', function () {
-  describe('NR_OF_RELATIONS', function () {
-    it('should pass through allenRelationBitPattern.NR_OF_RELATIONS', function () {
-      NR_OF_RELATIONS.should.equal(BITPATTERN_NR_OF_RELATIONS)
-    })
-  })
-  describe('BasicAllenRelation', function () {
-    describe('BASIC_RELATIONS', function () {
-      it('is an array', function () {
-        BasicAllenRelation.BASIC_RELATIONS.should.be.an.Array()
-      })
-      it('has 13 entries', function () {
-        BasicAllenRelation.BASIC_RELATIONS.length.should.equal(13)
-      })
-      it('contains only BasicAllenRelations', function () {
-        BasicAllenRelation.BASIC_RELATIONS.forEach(br => {
-          br.should.be.instanceof(BasicAllenRelation)
-        })
-      })
-      it('has no duplicates, and this is a basis', function () {
-        BasicAllenRelation.BASIC_RELATIONS.forEach((br1, i1) => {
-          BasicAllenRelation.BASIC_RELATIONS.forEach((br2, i2) => {
-            if (i1 < i2) {
-              br1.should.not.equal(br2)
-              // MUDO
-              // br1.implies(br2).should.be.false()
-              // br2.implies(br1).should.be.false()
-            }
-          })
-        })
-      })
-      it('has the basic relation at the position of its ordinal', function () {
-        BasicAllenRelation.BASIC_RELATIONS.forEach((br, i) => {
-          br.ordinal().should.equal(i)
-        })
-      })
-    })
-    testBasicRelation('PRECEDES', BasicAllenRelation.PRECEDES, 0, 'p')
-    testBasicRelation('MEETS', BasicAllenRelation.MEETS, 1, 'm')
-    testBasicRelation('OVERLAPS', BasicAllenRelation.OVERLAPS, 2, 'o')
-    testBasicRelation('FINISHED_BY', BasicAllenRelation.FINISHED_BY, 3, 'F')
-    testBasicRelation('CONTAINS', BasicAllenRelation.CONTAINS, 4, 'D')
-    testBasicRelation('STARTS', BasicAllenRelation.STARTS, 5, 's')
-    testBasicRelation('EQUALS', BasicAllenRelation.EQUALS, 6, 'e')
-    testBasicRelation('STARTED_BY', BasicAllenRelation.STARTED_BY, 7, 'S')
-    testBasicRelation('DURING', BasicAllenRelation.DURING, 8, 'd')
-    testBasicRelation('FINISHES', BasicAllenRelation.FINISHES, 9, 'f')
-    testBasicRelation('OVERLAPPED_BY', BasicAllenRelation.OVERLAPPED_BY, 10, 'O')
-    testBasicRelation('MET_BY', BasicAllenRelation.MET_BY, 11, 'M')
-    testBasicRelation('PRECEDED_BY', BasicAllenRelation.PRECEDED_BY, 12, 'P')
-    describe('RELATIONS', function () {
-      it('is an array', function () {
-        BasicAllenRelation.RELATIONS.should.be.an.Array()
-      })
-      it('contains the exact amount of instances', function () {
-        BasicAllenRelation.RELATIONS.length.should.equal(NR_OF_RELATIONS)
-      })
-      it('contains only AllenRelations', function () {
-        BasicAllenRelation.RELATIONS.forEach(gr => {
-          gr.should.be.instanceof(AllenRelation)
-        })
-      })
-      it('does not contain duplicates', function () {
-        /* IDEA A naive implementation iterates 67 108 864 times, and takes several minutes. This is optimized using a
-           Set. */
-        const gathering = new Set<AllenRelation>()
-
-        BasicAllenRelation.RELATIONS.forEach((gr, i) => {
-          gathering.size.should.equal(i)
-          gathering.has(gr).should.be.false()
-          gathering.add(gr)
-        })
-      })
-      it('contains all basic relations', function () {
-        BasicAllenRelation.BASIC_RELATIONS.forEach(br => {
-          BasicAllenRelation.RELATIONS.includes(br)
-        })
-      })
-    })
-  })
-  describe('special relations', function () {
-    describe('EMPTY', function () {
-      it('is an AllenRelation', function () {
-        EMPTY.should.be.instanceof(AllenRelation)
-      })
-      it('is not implied by anything', function () {
-        BasicAllenRelation.RELATIONS.filter(ar => ar !== EMPTY).forEach(ar => {
-          EMPTY.impliedBy(ar).should.be.false()
-        })
-      })
-    })
-    describe('FULL', function () {
-      it('is an AllenRelation', function () {
-        FULL.should.be.instanceof(AllenRelation)
-      })
-      it('is implied by everything', function () {
-        BasicAllenRelation.RELATIONS.forEach(ar => {
-          FULL.impliedBy(ar).should.be.true()
-        })
-      })
-    })
-  })
+  generateRelationTests('AllenRelation', AllenRelation, [
+    { name: 'PRECEDES', representation: 'p' },
+    { name: 'MEETS', representation: 'm' },
+    { name: 'OVERLAPS', representation: 'o' },
+    { name: 'FINISHED_BY', representation: 'F' },
+    { name: 'CONTAINS', representation: 'D' },
+    { name: 'STARTS', representation: 's' },
+    { name: 'EQUALS', representation: 'e' },
+    { name: 'STARTED_BY', representation: 'S' },
+    { name: 'DURING', representation: 'd' },
+    { name: 'FINISHES', representation: 'f' },
+    { name: 'OVERLAPPED_BY', representation: 'O' },
+    { name: 'MET_BY', representation: 'M' },
+    { name: 'PRECEDED_BY', representation: 'P' }
+  ])
 })
