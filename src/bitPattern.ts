52a6e5ce230ccea397ac8a9416b645b62b0ae514
Jan Dockx <jandockx@gmail.com>
2022-11-01 15:04:06 +0100

    test and fix bitPattern functions
    add preconditions
    there are unintuitive limitations

--- src/bitPattern.ts
+++ src/bitPattern.ts
@@ -1,30 +1,55 @@
 /**
  * The total number of relations, i.e., <code>2<sup>nrOfBits</sup></code>.
  */
+import assert from 'assert'
+
+/* limited by
+
+   - number size (< 2^(32 - 1) < Math.log2(Number.MAX_SAFE_INTEGER),
+   - address space for arrays, and
+   - reasonability
+
+   only. See also `bitCount`.  */
+export const largestNrOfBits = 16
+
 export function nrOfRelations (nrOfBits: number): number {
+  assert(Number.isInteger(nrOfBits))
+  assert(nrOfBits >= 0)
+  assert(nrOfBits <= largestNrOfBits)
+
   return Math.pow(2, nrOfBits)
 }
 
 export const EMPTY_BIT_PATTERN = 0 // 00000
 
 export function fullBitPattern (nrOfBits: number): number {
+  // preconditions in nrOfRelations
+
   return nrOfRelations(nrOfBits) - 1
 }
 
 export function relationBitPatterns (nrOfBits: number): readonly number[] {
+  // preconditions in nrOfRelations
+
   return [...Array(nrOfRelations(nrOfBits)).keys()]
 }
 
 export function isRelationBitPattern (nrOfBits: number, candidate: unknown): candidate is number {
+  // preconditions in nrOfRelations
+
   return (
     typeof candidate === 'number' &&
     Number.isInteger(candidate) &&
     candidate >= 0 &&
-    candidate <= nrOfRelations(nrOfBits) - 1
+    candidate <= fullBitPattern(nrOfBits)
   )
 }
 
 export function basicRelationBitPatterns (nrOfBits: number): readonly number[] {
+  assert(Number.isInteger(nrOfBits))
+  assert(nrOfBits >= 0)
+  assert(nrOfBits <= largestNrOfBits)
+
   return [...Array(nrOfBits).keys()].map(nr => Math.pow(2, nr))
 }
 
@@ -32,6 +57,8 @@ export function basicRelationBitPatterns (nrOfBits: number): readonly number[] {
  * A basic relation is expressed by a single bit in the bit pattern.
  */
 export function isBasicRelationBitPattern (nrOfBits: number, candidate: unknown): candidate is number {
+  // preconditions in isRelationBitPattern
+
   /* http://graphics.stanford.edu/~seander/bithacks.html
    * Determining if an integer is a power of 2
    * unsigned int v; // we want to see if v is a power of 2
@@ -44,7 +71,13 @@ export function isBasicRelationBitPattern (nrOfBits: number, candidate: unknown)
   return isRelationBitPattern(nrOfBits, candidate) && candidate !== 0 && (candidate & (candidate - 1)) === 0
 }
 
+export const largestBitInteger = 2 ** (32 - 1)
+
 export function bitCount (n: number): number {
+  assert(Number.isInteger(n))
+  assert(n >= 0)
+  assert(n <= largestBitInteger) // otherwise the algoritm flips the sign bit
+
   let count = 0
   while (n > 0) {
     n &= n - 1
---
+++ test/50.bitPattern.test.ts
@@ -0,0 +1,178 @@
+/* eslint-env mocha */
+
+import should from 'should'
+import { stuffWithUndefined } from './_stuff'
+import { inspect } from 'util'
+import {
+  largestNrOfBits,
+  nrOfRelations,
+  EMPTY_BIT_PATTERN,
+  fullBitPattern,
+  relationBitPatterns,
+  isRelationBitPattern,
+  basicRelationBitPatterns,
+  isBasicRelationBitPattern,
+  bitCount,
+  largestBitInteger
+} from '../src/bitPattern'
+
+const nrOfBitsCases = [0, 1, 2, 5, 13, largestNrOfBits]
+const bitCountCases = [0, 1, 3, 5, 13, 17, 978562897, largestBitInteger - 1, largestBitInteger]
+
+describe('bitPattern', function () {
+  it('EMPTY_BIT_PATTERN is 0', function () {
+    EMPTY_BIT_PATTERN.should.equal(0)
+  })
+  nrOfBitsCases.forEach(nrOfBits => {
+    describe(`${nrOfBits} bits`, function () {
+      describe('nrOfRelations', function () {
+        it(`returns 2^${nrOfBits}`, function () {
+          const result = nrOfRelations(nrOfBits)
+          Number.isInteger(result).should.be.true()
+          Math.log2(result).should.equal(nrOfBits)
+        })
+      })
+      describe('fullbitPattern', function () {
+        it(`returns 2^${nrOfBits} - 1, and is all 1's`, function () {
+          const result = fullBitPattern(nrOfBits)
+          Number.isInteger(result).should.be.true()
+          Math.log2(result + 1).should.equal(nrOfBits)
+          const s = result.toString(2).split('')
+          if (nrOfBits <= 0) {
+            s.length.should.equal(1)
+            s[0].should.equal('0')
+          } else {
+            s.length.should.equal(nrOfBits)
+            s.forEach(b => {
+              b.should.equal('1')
+            })
+          }
+        })
+      })
+      describe('relation bit patterns', function () {
+        before(function () {
+          this['relationBitPatterns'] = relationBitPatterns(nrOfBits)
+          this['basicRelationBitPatterns'] = basicRelationBitPatterns(nrOfBits)
+        })
+
+        describe('relationBitPatterns', function () {
+          it('is an array', function () {
+            const rbps: number[] = this['relationBitPatterns']
+            rbps.should.be.an.Array()
+          })
+          it('contains the exact amount of numbers', function () {
+            const rbps: number[] = this['relationBitPatterns']
+            rbps.length.should.equal(nrOfRelations(nrOfBits))
+          })
+          it(`each element has at most ${nrOfBits} its`, function () {
+            const rbps: number[] = this['relationBitPatterns']
+            rbps.forEach(bp => {
+              const s = bp.toString(2).split('')
+              if (bp === 0) {
+                s.length.should.equal(1)
+                s[0].should.equal('0')
+              } else {
+                s.length.should.be.lessThanOrEqual(nrOfBits)
+              }
+            })
+          })
+          it('contains the pattern of the index at each location', function () {
+            const rbps: number[] = this['relationBitPatterns']
+            rbps.forEach((bp, i) => {
+              should(bp).equal(i)
+            })
+          })
+        })
+        describe('isRelationBitPattern', function () {
+          it('returns true for all bit patterns', function () {
+            const rbps: number[] = this['relationBitPatterns']
+            rbps.forEach(bp => {
+              isRelationBitPattern(nrOfBits, bp).should.be.true()
+            })
+          })
+
+          stuffWithUndefined
+            .filter(s => s !== 0 && s !== 1)
+            .forEach(s => {
+              it(`returns false for ${JSON.stringify(s)}`, function () {
+                isRelationBitPattern(nrOfBits, s).should.be.false()
+              })
+            })
+        })
+
+        describe('basicRelationBitPatterns', function () {
+          it('is an array', function () {
+            const brbps: number[] = this['basicRelationBitPatterns']
+            brbps.should.be.an.Array()
+          })
+          it('contains the exact amount of numbers', function () {
+            const brbps: number[] = this['basicRelationBitPatterns']
+            brbps.length.should.equal(nrOfBits)
+          })
+          it('each elemant has 1 1-bit', function () {
+            const brbps: number[] = this['basicRelationBitPatterns']
+            brbps.forEach(bp => {
+              const s = bp.toString(2).split('')
+              s.length.should.be.lessThanOrEqual(nrOfBits)
+              s.filter(d => d === '1').length.should.equal(1)
+            })
+          })
+          it('contains the pattern of the index at each location', function () {
+            const brbps: number[] = this['basicRelationBitPatterns']
+            brbps.forEach((bp, i) => {
+              should(bp).equal(Math.pow(2, i))
+            })
+          })
+        })
+        describe('isBasicRelationBitPattern', function () {
+          it('returns true for all basic patterns', function () {
+            const brbps: number[] = this['basicRelationBitPatterns']
+            brbps.forEach(bbp => {
+              isBasicRelationBitPattern(nrOfBits, bbp).should.be.true()
+            })
+          })
+          it('returns false for all non-basic bit patterns', function () {
+            const rbps: number[] = this['relationBitPatterns']
+            const brbps: number[] = this['basicRelationBitPatterns']
+            rbps
+              .filter(bp => !brbps.map(bbp => bbp).includes(bp))
+              .forEach(nonBasicBitPattern => {
+                isBasicRelationBitPattern(nrOfBits, nonBasicBitPattern).should.be.false()
+              })
+          })
+
+          stuffWithUndefined
+            .filter(s => s !== 1)
+            .concat([
+              NaN,
+              Number.POSITIVE_INFINITY,
+              Number.NEGATIVE_INFINITY,
+              Number.EPSILON,
+              Number.MAX_VALUE,
+              Number.MIN_VALUE,
+              Number.MAX_SAFE_INTEGER,
+              Number.MIN_SAFE_INTEGER,
+              nrOfRelations(nrOfBits) + 1,
+              Math.pow(2, largestNrOfBits + 2)
+            ])
+            .forEach(s => {
+              it(`returns false for ${inspect(s)}`, function () {
+                isBasicRelationBitPattern(nrOfBits, s).should.be.false()
+              })
+            })
+        })
+      })
+    })
+  })
+  describe('bitCount', function () {
+    bitCountCases.forEach(n => {
+      it(`works for ${n}`, function () {
+        const result = bitCount(n)
+        Number.isInteger(result).should.be.true()
+        result.should.be.greaterThanOrEqual(0)
+        const s = n.toString(2).split('')
+        result.should.equal(s.filter(d => d === '1').length)
+      })
+    })
+  })
+})
