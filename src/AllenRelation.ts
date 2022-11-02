8bbd07dbf9229059646f03c109772eb9d69bde29
Jan Dockx <jandockx@gmail.com>
2022-11-02 10:03:24 +0100

    tweak STARTS doc

--- src/AllenRelation.ts
+++ src/AllenRelation.ts
@@ -153,21 +153,19 @@ export class AllenRelation extends Relation {
   // Bit pattern: 16 = '0000000010000'
 
   /**
-   * A <strong>basic</strong> time interval relation that says that an interval
-   * <var>I1</var> <dfn>starts</dfn> an interval <var>I2</var>, i.e., the
-   * begin of <var>I1</var> is the begin of <var>I2</var>, and the end of
-   * <var>I1</var> is earlier than the end of <var>I2</var>:
+   * A _basic_ Allen relation that says that an interval `I1` _starts_ an interval `I2`, i.e.,
+   *
+   * - the `start` of `I1` is the `start` of `I2`, and
+   * - the `end` of `I1` is before the `end` of `I2`
    *
    * ```
-   * (I1.begin != null) &amp;&amp; (I1.end != null) &amp;&amp; (I2.begin != null) &amp;&amp; (I2.end != null) &amp;&amp;
-   *   (I1.begin == I2.begin) &amp;&amp; (I1.end &lt; I2.end)
+   * (I1.start ≠ undefined) ∧ (I1.end ≠ undefined) ∧ (I2.start ≠ undefined) ∧ (I2.end ≠ undefined) ∧
+   *   (I1.start = I2.start) ∧ (I2.end < I1.end)
    * ```
    *
-   * <img style="text-align: center;" src="doc-files/AllenRelation-starts.png">
+   * ![starts](https://bitbucket.org/toryt/allen/raw/c00cab429681246b7718a462b94c4a68094e967c/doc/AllenRelation-starts.png)
    *
-   * The conventional short representation of this Allen relation is &quot;<code><strong>s</strong></code>&quot;.
-   *
-   * The converse of this relation is {@link STARTED_BY}.
+   * The short representation of this Allen relation is `s`. The converse of this relation is {@link STARTED_BY}.
    */
   static readonly STARTS: AllenRelation = AllenRelation.BASIC_RELATIONS[5]
   // Bit pattern: 32 = '0000000100000'
