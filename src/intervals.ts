9ac58f49314d026793c21aaf04c9cf4081181d0f
Jan Dockx <jandockx@gmail.com>
2022-11-29 01:22:11 +0100

    describe interSectionSequence

--- src/intervals.ts
+++ src/intervals.ts
@@ -14,6 +14,34 @@
  limitations under the License.
  */
 
+import { Interval } from './Interval'
+import { Comparator } from './Comparator'
+
+export interface SourceIntervals<T> {
+  [reference: string]: Array<Interval<T>>
+}
+
+/**
+ * Return a sequence that has the fewest possible intervals, that are intersections of intervals provided in the
+ * `sources`, so that the every interval in the `sources` is “covered” by a gapless subsequence of one or more intervals
+ * in the result.
+ *
+ * Each interval in the result `ir` refers to the intervals `is` in the `sources` in its `referenceIntervals` that cause
+ * it to appear (`ir (sedf) is`). `ir` {@link AllenRelation.DOES_NOT_CONCUR_WITH does not concurr with} any other
+ * interval in the sources (`(pmMP)`) . In other words, the relation of an interval of the result with any interval in
+ * the sources is implied by `(pmsedfMP)`.
+ *
+ * Since the `sources` do not have to be sequences themselves, one interval in the result might be the intersection of
+ * more than one interval of the same `source`.
+ *
+ * MUDO so the whole naming the sources thing does not make sense? It sure does in usage! But each reference can be an
+ * array!
+ *
+ * The resulting sequence is ordered, might have gaps, and might be left- and / or right-indefinite.
+ */
+export function interSectionSequence<T> (sources: SourceIntervals<T>, compareFn?: Comparator<T>): Interval<T>[] {
+  return []
+}
 // /**
 //  * Turn the _set_ of intervals `i` into a {@link isSequence seqyence}.
 //  *
