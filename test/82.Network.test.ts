d8df75b4da95dd477e099da1f08886a6b94bca22
Jan Dockx <jandockx@gmail.com>
2023-02-05 11:09:21 +0100

    test Network constructor

--- test/82.Network.test.ts
+++ test/82.Network.test.ts
@@ -1,12 +1,12 @@
 /*
  Copyright © 2022 – 2023 by Jan Dockx
-
+ 
  Licensed under the Apache License, Version 2.0 (the “License”);
  you may not use this file except in compliance with the License.
  You may obtain a copy of the License at
-
+ 
  http://www.apache.org/licenses/LICENSE-2.0
-
+ 
  Unless required by applicable law or agreed to in writing, software
  distributed under the License is distributed on an “AS IS” BASIS,
  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,8 +18,17 @@
 
 import { AllenRelation } from '../src'
 import { Network } from '../src/Network'
+import should from 'should'
 
-describe('compose', function () {
+describe('Network', function () {
+  describe('constructor', function () {
+    it('can be constructed', function () {
+      const subject: Network = new Network()
+      should(subject).be.instanceof(Network)
+      subject.get('a', 'b').should.equal(AllenRelation.FULL)
+      subject.get('c', 'c').should.equal(AllenRelation.EQUALS)
+    })
+  })
   // it('composes', function () {
   //   const Rintersection = AllenRelation.fromString<AllenRelation>('sedf')
   //   console.log(`Rintersection: ${Rintersection.toString()}`)
